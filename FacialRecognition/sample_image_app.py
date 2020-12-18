652559a20dd63b442b0bdddc90fb1b7d6b30cfbc
Michael-Santoro <74157573+Michael-Santoro@users.noreply.github.com>
2020-12-17 20:03:03 -0700

    Update sample_image_app.py
    Added ability to check for directory and increments capture by 25 so that multiple light conditions can be leveraged.

--- FacialRecognition/sample_image_app.py
+++ FacialRecognition/sample_image_app.py
@@ -1,21 +1,27 @@
 import numpy
 import cv2
+import os
 from os import makedirs
 from matplotlib import pyplot
 from matplotlib.image import imread
 #Define Dog Name
 dogName = 'Aila'
-path = 'C:/Users/micha/Machine Learning/FacialRecognitionV1/FacialRecognition'
-
+rangeStart = 0
+rangeStop = 25
 # create directories for raw image capture
 dataset_home = dogName+'/'
-makedirs(dataset_home, exist_ok=True)
-writeLocation = path + dataset_home
+if os.path.isfile(dataset_home):
+	makedirs(dataset_home, exist_ok=True)
+else:
+	file_list = os.listdir(dataset_home)
+	rangeStart = len(file_list)
+	rangeStop = len(file_list) + 25
+
 # capture images
 camera = cv2.VideoCapture(0)
-for i in range(10):
+for i in range(25):
     return_value, image = camera.read()
-    cv2.imwrite(writeLocation+'Aila'+str(i)+'.png', image)
+    cv2.imwrite(dataset_home+'Aila'+str(i)+'.png', image)
 del(camera)
 
 
