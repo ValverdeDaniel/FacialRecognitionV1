5353790f8ff1c5311e9b9db9bcef0b07235922ec
Michael-Santoro <74157573+Michael-Santoro@users.noreply.github.com>
2020-12-20 19:10:43 -0700

    Edits

--- FacialRecognition/re-sizing.py
+++ FacialRecognition/re-sizing.py
@@ -5,15 +5,16 @@ from numpy import asarray
 from numpy import save
 import cv2
 # define location of dataset
-folder = 'train/'
+
 photos, labels = list(), list()
 # find the size of the image
 img = cv2.imread('Aila/Aila0.png')
 height, width, channels = img.shape
 
 #Center Section Algorithm (200 x 200)
-for file in listdir(folder):
+for file in listdir('Aila'):
 	y = (width-height)//2
-	scaled = file[:, y:y+height]
+	img = cv2.imread('Aila/'+file)
+	scaled = img[:, y:y+height]
 	scaled = cv2.resize(scaled,(200,200))
-	cv2.imwrite(dataset_home+'Aila'+str(i)+'.png', scaled)
+	cv2.imwrite('+'Aila'+str(i)+'.png', scaled)
--- FacialRecognition/sample_image_app.py
+++ FacialRecognition/sample_image_app.py
@@ -4,25 +4,31 @@ import os
 from os import makedirs
 from matplotlib import pyplot
 from matplotlib.image import imread
+import time
+
 #Define Dog Name
 dogName = 'Aila'
 rangeStart = 0
-rangeStop = 25
+rangeStop = 9
 # create directories for raw image capture
 dataset_home = dogName+'/'
-if os.path.isfile(dataset_home):
-	makedirs(dataset_home, exist_ok=True)
-else:
-	file_list = os.listdir(dataset_home)
-	rangeStart = len(file_list)
-	rangeStop = len(file_list) + 25
+#Checks if the directory
+makedirs(dataset_home, exist_ok=True)
+# if os.path.isfile(dataset_home):
+# 	makedirs(dataset_home, exist_ok=True)
+# else:
+# 	file_list = os.listdir(dataset_home)
+# 	rangeStart = len(file_list)
+# 	rangeStop = len(file_list) + 25
 
 # capture images
 camera = cv2.VideoCapture(0) #set to 1 for USB Connected Camera
-for i in range(25):
+for i in range(9):
     return_value, image = camera.read()
     cv2.imwrite(dataset_home+'Aila'+str(i)+'.png', image)
+	time.sleep(1)
 del(camera)
+
 
 
 # plot dog photos
@@ -31,7 +37,7 @@ for i in range(9):
 	# define subplot
 	pyplot.subplot(330 + 1 + i)
 	# define filename
-	filename = dataset_home + dogName + '.' + str(i) + '.png'
+	filename = dataset_home + dogName + str(i) + '.png'
 	# load image pixels
 	image = imread(filename)
 	# plot raw pixel data
